168bc9a1b59275df6c0ede6c4edd4842a9915651
Jeffrey Ryan Walker <jryanwalker93@gmail.com>
2020-09-12 17:10:38 -0500

    Renders app to page

--- client/App.jsx
+++ client/App.jsx
@@ -1,1 +1,6 @@
 //test comment made by MA
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Homepage from './components/Homepage.jsx';
+
+ReactDOM.render(<Homepage />, document.getElementById('root'));
