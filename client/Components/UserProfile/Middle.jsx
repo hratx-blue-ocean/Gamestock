5f85b9732e31e36ac5d5fee62c63c5fc29b8b22a
andrewbinkard <andrewbinkard@gmail.com>
2020-09-14 16:29:36 -0500

    pass props to the correct components

--- client/Components/UserProfile/Middle.jsx
+++ client/Components/UserProfile/Middle.jsx
@@ -1,59 +1,20 @@
 import React, { useState } from "react";
+import UserProfile from "./UserProfile.jsx";
 import DisplayItems from "./DisplayItems.jsx";
 import DisplayItemsValue from "./DisplayItemsValue.jsx";
 import Print from "./Print.jsx";
 import CollectionList from ".CollectionList.jsx";
 
-const Middle = () => {
-  // useState to store the array of returned data from items table
-  const [items, setItems] = useState([]);
-  // useState to store the array of returned data from items_value_by_date
-  const [prices, setPrices] = useState([]);
-  // useState to store the array of returned data from users
-  const [users, setUsers] = useState([]);
-
-  // data needed for these child components
-  // total value and total number of items of the colleciton currently being displayed (displayItems and displayItemsValue)
-  // game thumbnail, game title, game price, console
-
-  axios.get("/userProfile/items").then((data) => {
-    console.log("ITEMS: ", data);
-    setItems(data);
-  });
-
-  axios.get("/userProfile/prices").then((data) => {
-    console.log("PRICES: ", data);
-    setPrices(data);
-  });
-
-  axios.get("/userProfile/users").then((data) => {
-    console.log("USERS: ", data);
-    setUsers(data);
-  });
-
-  // const getItems = () => {
-  //   return axios.get("/userProfile/items");
-  // };
-
-  // const getPrices = () => {
-  //   return axios.get("userProfile/prices");
-  // };
-
-  // axios.all([getItems(), getPrices()]).then(
-  //   axios.spread((items, prices) => {
-  //     console.log("ITEMS: ", items);
-  //     console.log("PRICES: ", prices);
-  //     // setItems(items);
-  //     // setPrices(prices);
-  //   })
-  // );
-
+const Middle = ({ users, prices, items }) => {
   return (
     <div>
-      <DisplayItems />
-      <DisplayItemsValue />
+      {/* these first two things are displaying the number of items and their combined value */}
+      <DisplayItems items={items} />
+      <DisplayItemsValue prices={prices} />
+      {/* print will be the print button that converts the information displayed into a csv file */}
       <Print />
-      <CollectionList />
+      {/* child of collectionList will be card */}
+      <CollectionList users={users} items={items} prices={prices} />
     </div>
   );
 };
