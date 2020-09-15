452550aab966c3da85733e561dc3eec3c2d4fb2e
Belle Nguyen <NgBelleK@gmail.com>
2020-09-15 01:26:30 -0500

    move AddItem forms to another component

--- client/Components/UserProfile/AddItem.jsx
+++ client/Components/UserProfile/AddItem.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
+import AddItemForm from "./AddItemForm.jsx";
 
 const AddItem = () => {
-  return <div></div>;
+  return <div>
+    < AddItemForm />
+  </div>;
 };
 
 export default AddItem;
