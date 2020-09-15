b8fd24fcebd2cc21d90412e46ec833436a7bd57b
Belle Nguyen <NgBelleK@gmail.com>
2020-09-15 18:22:41 -0500

    add unique key to searched items

--- client/Components/UserProfile/AddItem.jsx
+++ client/Components/UserProfile/AddItem.jsx
@@ -2,12 +2,11 @@ import React, { useState, useEffect } from "react";
 import AddItemForm from "./AddItemForm.jsx";
 
 const AddItem = () => {
-
   return (
-  <div>
-    < AddItemForm />
-  </div>
-  )
+    <div>
+      <AddItemForm />
+    </div>
+  );
 };
 
 export default AddItem;
--- client/Components/UserProfile/AddItemList.jsx
+++ client/Components/UserProfile/AddItemList.jsx
@@ -1,17 +1,20 @@
 import React, { useState, useEffect } from "react";
-import AddItemSingleItem from "./AddItemSingleItem.jsx"
+import AddItemSingleItem from "./AddItemSingleItem.jsx";
 const AddItemList = (props) => {
   return (
     <div>
       {props.items.map((itemObj) => {
-        return <AddItemSingleItem item={itemObj} select={(item) => {props.select(item)}}/>;
+        return (
+          <AddItemSingleItem
+            key={itemObj.id}
+            item={itemObj}
+            select={(item) => {
+              props.select(item);
+            }}
+          />
+        );
       })}
     </div>
-    // <div>
-    //     <AddItemSingleItem />
-    //     <AddItemSingleItem />
-    //     <AddItemSingleItem />
-    // </div>
   );
 };
 
