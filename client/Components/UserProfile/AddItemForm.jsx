ed38fae08a4db2a4c1aefb82875845a2070f4328
Belle Nguyen <NgBelleK@gmail.com>
2020-09-15 12:35:31 -0500

    work on connecting add item list

--- client/Components/UserProfile/AddItemForm.jsx
+++ client/Components/UserProfile/AddItemForm.jsx
@@ -3,6 +3,7 @@ import AddItemList from "./AddItemList.jsx";
 import NewItemSearchBar from "./NewItemSearchBar.jsx";
 
 const AddItemForm = ({ submitInfo }) => {
+  const [itemSelected, setItemSelected] = useState({});
   const [dateAcquired, setDateAcquired] = useState("");
   const [purchasedPrice, setPurchasedPrice] = useState(0);
   const [itemNotes, setItemNotes] = useState("");
