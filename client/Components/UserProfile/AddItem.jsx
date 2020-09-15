1f984aef842b330de34d98bd0b7ec1ce5a5b0160
Belle Nguyen <NgBelleK@gmail.com>
2020-09-15 11:01:42 -0500

    combine data to addItem

--- client/Components/UserProfile/AddItem.jsx
+++ client/Components/UserProfile/AddItem.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from "react";
 import AddItemForm from "./AddItemForm.jsx";
 
 const AddItem = () => {
+  const [newItemInfo, setNewItemInfo] = useState(null);
+
   return <div>
-    < AddItemForm />
+    < AddItemForm submitInfo={(info) => setNewItemInfo(info)} />
   </div>;
 };
 
