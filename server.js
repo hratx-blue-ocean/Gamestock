0d676e41c1cce3acd919529f7acf4505fe622e7f
Danielle Blom <dlblom91@gmail.com>
2020-09-15 10:35:14 -0500

    IGDB API call to get games by query

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-//imports acces token for jwt from .env file
-require('dotenv').config();
-
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
@@ -19,70 +16,11 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/login', (req, res) => {
-  let username = req.body.username;
-  const token = jwt.sign({ username }, process.env.ACCESS_TOKEN, {
-    algorithm: 'HS256',
-    expiresIn: jwtExpirySeconds,
-  });
-
-  Users.get({username})
-    .then((user) => {
-      if(!user) {
-      //username not found
-      console.log('Username at login not found');
-      res.status(200).send('User not found. Sign up');
-      } else {
-        //user exists, verify password
-        return Users.compare(req.body.password, user.hash)
-      }
-    })
-    .then((verification) => {
-      //user exists but entered incorrect password
-      if(!verification) {
-        console.log('Incorrect password entered');
-        res.status(401).send('Incorrect password entered, try again');
-      } else {
-        //user entered the correct password
-        console.log('User can log in');
-
-        //create auth cookie
-        console.log("token:", token);
-        res.cookie('token', token, { maxAge: jwtExpirySeconds * 1000 });
-        res.status(200).send('Successful login');
-      }
-    })
-    .catch((err) => {
-      console.log('Error. Could not log in user: ', err);
-      res.status(500)
-    })
-})
-
 app.get('/signup', (req, res) => {
-  //req.body should include username, avatar and password
-  let username = req.body.username;
-
-  //check if username exists
-  Users.get({username})
-    .then((user) => {
-      if (user) {
-        res.status(200).send('Username already exists');
-      } else {
-        //if user does not exist, create one
-        Users.create(req.body)
-        .then((response) => {
-          res.status(200).send(response)
-        })
-        .catch( (err) => {
-          console.log('Error creating user', err);
-          res.status(500).send(err);
-        })
-      }
-    })
-    .catch((err) => {
-      console.log('Error in server reading username: ', err);
-      res.status(500).send(err);
-    })
+ //req.body should include username, avatar and password
+  Users.create(req.body)
+    .then( (response) => res.send(response))
+    .catch( (err) => {res.send(err)})
 });
 
 app.get('/signup', (req, res) => {
@@ -92,14 +30,13 @@ app.get('/signup', (req, res) => {
     .catch( (err) => {res.send(err)})
 });
 
-// IGDB API Get request by keyword
+// IGDB API Get top 10 items by request by keyword
 app.get(`/items`, (req, res) => {
-  let query = req.body;
+  let query = 'The Witcher 3';
   axios({
-    url: `https://api-v3.igdb.com/games/?search=${query}&fields=name,platforms,cover,artworks,summary`,
+    url: `https://api-v3.igdb.com/games/?search=${query}&fields=name,platforms,cover,summary`,
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
         'user-key': IGDBkey
     }
   })
@@ -112,33 +49,14 @@ app.get(`/items`, (req, res) => {
     });
 })
 
-app.get(`/platform`, (req, res) => {
-  axios({
-    url: `https://api-v3.igdb.com/platforms`,
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'user-key': IGDBkey
-    },
-  })
-    .then(response => {
-        res.status(200).send(response.data);
-        console.log(response.data)
-    })
-    .catch(err => {
-        console.error(err);
-    });
-})
-
-// app.get(`/platforms`, (req, res) => {
+// app.get(`/itemImage`, (req, res) => {
 //   axios({
-//     url: `https://api-v3.igdb.com/platforms`,
+//     url: `https://api-v3.igdb.com/covers/?fields=alpha_channel,animated,checksum,game,height,image_id,url,width`,
 //     method: 'POST',
 //     headers: {
-//         'Accept': 'application/json',
-//         'user-key': IGDBkey
-//     },
-//     data: "fields generation,name,product_family,slug,summary;"
+//         'user-key': IGDBkey,
+//         'content-type': 'text/plain'
+//     }
 //   })
 //     .then(response => {
 //         res.status(200).send(response.data);
@@ -150,5 +68,5 @@ app.get(`/platform`, (req, res) => {
 // })
 
 app.listen(port, ()=> {
-  console.log('listening in on port ', port);
-});
+    console.log('listening in on port ', port);
+})
