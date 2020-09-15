9a868144fb5cb75fff0c5952111faf216b47b132
Steven <stevenmbrotherton@gmail.com>
2020-09-15 10:54:22 -0500

    removed .env

--- client/App.jsx
+++ client/App.jsx
@@ -1,7 +1,9 @@
 //test comment made by MA
 import React from "react";
 import ReactDOM from "react-dom";
-import Homepage from "./components/Homepage/Homepage.jsx";
+import Homepage from "./Components/Homepage/Homepage.jsx";
+
+// ReactDOM.render(<Homepage />, document.getElementById("root"));
 import UserProfile from "./Components/UserProfile/UserProfile.jsx";
 import { GlobalStyles } from "./components/Core/coreStyles.jsx";
 
