9da008f750dc6078dfd3cae4c603778404b601b5
Belle Nguyen <NgBelleK@gmail.com>
2020-09-15 11:01:42 -0500

    combine data to addItem

--- client/Components/UserProfile/AddItemForm.jsx
+++ client/Components/UserProfile/AddItemForm.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from "react";
 
-const AddItemForm = () => {
+const AddItemForm = ({ submitInfo }) => {
   const [dateAcquired, setDateAcquired] = useState("");
   const [purchasedPrice, setPurchasedPrice] = useState(0);
   const [itemNotes, setItemNotes] = useState("");
   const [itemCondition, setItemCondition] = useState("New");
   const [isTradeable, setIsTradeable] = useState(false);
 
+  const submittedInfo = {
+    dateAcquired: dateAcquired,
+    purchasedPrice: purchasedPrice,
+    itemNotes: itemNotes,
+    itemCondition: itemCondition,
+    isTradeable: isTradeable,
+  };
   return (
     <div>
       <div>
@@ -30,12 +37,17 @@ const AddItemForm = () => {
           <form>
             {/* date when item was bought */}
             <label htmlFor="dateAcquired">Date Acquired:</label>
-            <input onChange={e => setDateAcquired(e.target.value)} type="date" id="start" value={dateAcquired}></input>
+            <input
+              onChange={(e) => setDateAcquired(e.target.value)}
+              type="date"
+              id="start"
+              value={dateAcquired}
+            ></input>
             <br></br>
             {/* price at purchase of item */}
             <label htmlFor="purchasedPrice">PurchasedPrice:</label>
             <input
-              onChange={e => setPurchasedPrice(e.target.value)}
+              onChange={(e) => setPurchasedPrice(e.target.value)}
               id="purchasedPrice"
               type="number"
               min="0.01"
@@ -47,7 +59,7 @@ const AddItemForm = () => {
             {/* notes for user comments */}
             <label htmlFor="comment">Notes:</label>
             <textarea
-              onChange={e => setItemNotes(e.target.value)}
+              onChange={(e) => setItemNotes(e.target.value)}
               rows="4"
               cols="50"
               id="comment"
@@ -55,7 +67,11 @@ const AddItemForm = () => {
             ></textarea>
             <br></br>
             <label htmlFor="ItemCondition">Item Condition:</label>
-            <select id="ItemCondition" value={itemCondition} onChange={(e) => setItemCondition(e.target.value)} >
+            <select
+              id="ItemCondition"
+              value={itemCondition}
+              onChange={(e) => setItemCondition(e.target.value)}
+            >
               <option value="New">New</option>
               <option value="Used">Used</option>
               <option value="Good">Good</option>
@@ -64,14 +80,19 @@ const AddItemForm = () => {
 
             <div>
               <label htmlFor="forTrade">tradeable</label>
-              <input onChange={e => setIsTradeable(!isTradeable)} type="checkbox" id="forTrade" checked={isTradeable}></input>
+              <input
+                onChange={(e) => setIsTradeable(!isTradeable)}
+                type="checkbox"
+                id="forTrade"
+                checked={isTradeable}
+              ></input>
             </div>
+            <button onClick={() => submitInfo(submittedInfo)} type="button">
+              Submit
+            </button>
           </form>
-
-
         </div>
 
-        <button type="button">Submit</button>
         <button type="button">Cancel</button>
       </div>
     </div>
