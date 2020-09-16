81124e688591e9b9d68bbd0018c4dfafe317cbe0
Mariella-Arias <ariasmariella.brm@gmail.com>
2020-09-16 12:13:43 -0500

    Creates a header.
    - Dynamically renders username upon login
    - dynamically renders signin/signup modal

--- client/App.jsx
+++ client/App.jsx
@@ -1,22 +1,19 @@
-//test comment made by MA
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Header from "./Components/Header/header.jsx";
+import Header from "./Components/Header/Header.jsx";
 import Homepage from "./Components/Homepage/Homepage.jsx";
 import UserProfile from "./Components/UserProfile/UserProfile.jsx";
 import { GlobalStyles } from "./Components/Core/coreStyles.jsx";
-<<<<<<< HEAD
 // drop this eventually
 import ExampleModal from "./Components/Core/ExampleModal.jsx";
-=======
-import Header from "./Components/Header.jsx";
->>>>>>> Implement a header
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("");
+  const [userId, setUserId] = useState(0);
+
   return (
-<<<<<<< HEAD
     <>
       <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Router>
@@ -29,12 +26,6 @@ function App() {
           </Route>
         </Switch>
       </Router>
-=======
-    <div>
-      <Header />
-      <Homepage />
-      <UserProfile />
->>>>>>> Implement a header
       <GlobalStyles />
     </>
   );
