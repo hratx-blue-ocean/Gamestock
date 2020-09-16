d34b9f11e6de8bb021383498f42bf7cd8aeaecb8
Belle Nguyen <NgBelleK@gmail.com>
2020-09-15 19:35:57 -0500

    Add Item form without Modal

--- client/Components/UserProfile/AddItemSingleItem.jsx
+++ client/Components/UserProfile/AddItemSingleItem.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 
 const AddItemSingleItem = (props) => {
-  // edit and delete button
   return <div>
       <p onClick={() => {props.select(props.item)}}>{props.item["console-name"]} {props.item["product-name"]}</p>
   </div>;
