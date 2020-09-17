76d53bd6c8079c9315dfffcbc76b4b02024c6376
Mike Champagne <geauxdrum@gmail.com>
2020-09-17 15:35:58 -0500

    adds radio button to core styles

--- client/Components/Core/coreStyles.jsx
+++ client/Components/Core/coreStyles.jsx
@@ -187,30 +187,28 @@ export const Dropdown = styled.select`
   }
 `;
 
-// export const StyledRadio = styled.div`
-//   input [type="radio"] {
-//     display: none;
-//     &:checked + label:before {
-//       border-color: #eb29fd;
-//       animation: ripple 0.1s linear forwards;
-//     }
-//     &:checked + label:after {
-//       transform: scale(1);
-//     }
-//   }
-// `;
+export const StyledRadio = styled.div`
+  margin: 10px;
+`;
 
-// export const RadioButtion = ({ label, value, id, isSelected, changed }) => {
-//   return (
-//     <StyledRadio>
-//       <input
-//         type="radio"
-//         id={id}
-//         onChange={changed}
-//         value={value}
-//         checked={isSelected}
-//       />
-//       <label htmlFor={id}>{label}</label>
-//     </StyledRadio>
-//   );
-// };
+// as long as you pass down the required props this will work
+// requires a custom onChange function that looks like this:
+
+// customRadioChangeHandler = (e) => {
+//   setCustomRadioVar(e.target.value)
+// }
+
+export default function RadioButton({ label, value, id, isSelected, changed }) {
+  return (
+    <StyledRadio>
+      <input
+        type="radio"
+        id={id}
+        onChange={changed}
+        value={value}
+        checked={isSelected}
+      />
+      <label htmlFor={id}>{label}</label>
+    </StyledRadio>
+  );
+}
